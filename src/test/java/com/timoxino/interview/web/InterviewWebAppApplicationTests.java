434f8d522a2d19c50b249872630aff8e1b526089
Tsimafei Shchytkavets <timoxino@gmail.com>
2022-09-08 22:49:40 -0400

    Initial commit

---
+++ src/test/java/com/timoxino/interview/web/InterviewWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timoxino.interview.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

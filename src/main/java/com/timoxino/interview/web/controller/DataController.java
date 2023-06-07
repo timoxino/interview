446270259826e98750caee377ae0640002f3f72b
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-06-07 18:32:16 -0400

    lookup parent attributes from db

--- src/main/java/com/timoxino/interview/web/controller/DataController.java
+++ src/main/java/com/timoxino/interview/web/controller/DataController.java
@@ -2,6 +2,9 @@ package com.timoxino.interview.web.controller;
 
 import com.timoxino.interview.web.model.DataNode;
 import com.timoxino.interview.web.repo.DataNodeRepository;
+
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,8 +25,18 @@ public class DataController {
     }
 
     @PostMapping
-    DataNode create(@RequestBody DataNode occupation) {
-        return dataNodeRepository.save(occupation);
+    DataNode create(@RequestBody DataNode dataNode) {
+        Assert.notNull(dataNode.getParent(), "Parent object info (name or id) must be passed");
+
+        if(StringUtils.hasText(dataNode.getParent().getName())) {
+            dataNodeRepository.findByName(dataNode.getParent().getName()).ifPresent(dataNode::setParent);
+        }
+
+        if(dataNode.getParent().getId() != null){
+            dataNodeRepository.findById(dataNode.getParent().getId()).ifPresent(dataNode::setParent);
+        }
+
+        return dataNodeRepository.save(dataNode);
     }
 
     @DeleteMapping("/{id}")

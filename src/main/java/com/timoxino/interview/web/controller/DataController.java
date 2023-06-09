4707e5b41e17903c5c4ba731c4bb58612a1fa88d
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-06-08 23:23:55 -0400

    add unit test for creating with parent id/name

--- src/main/java/com/timoxino/interview/web/controller/DataController.java
+++ src/main/java/com/timoxino/interview/web/controller/DataController.java
@@ -29,16 +29,12 @@ public class DataController {
     DataNode create(@RequestBody DataNode dataNode) throws ParentDetailsMissingException {
         try {
             Assert.notNull(dataNode.getParent(), ParentDetailsMissingException.message);
-
-            if (StringUtils.hasText(dataNode.getParent().getName())) {
-                dataNodeRepository.findByName(dataNode.getParent().getName()).ifPresent(dataNode::setParent);
-            }
-
             if (dataNode.getParent().getId() != null) {
                 dataNodeRepository.findById(dataNode.getParent().getId()).ifPresent(dataNode::setParent);
+            } else if (StringUtils.hasText(dataNode.getParent().getName())) {
+                dataNodeRepository.findByName(dataNode.getParent().getName()).ifPresent(dataNode::setParent);
             }
-
-            dataNode =  dataNodeRepository.save(dataNode);
+            dataNode = dataNodeRepository.save(dataNode);
         } catch (IllegalArgumentException exception) {
             throw new ParentDetailsMissingException();
         }

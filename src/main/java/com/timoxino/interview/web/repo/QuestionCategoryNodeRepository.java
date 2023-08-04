4360df89352f7bbd94077f54a9dfc17a29cebdec
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-08-04 16:43:26 -0400

    remove question category/complexity before adding new

--- src/main/java/com/timoxino/interview/web/repo/QuestionCategoryNodeRepository.java
+++ src/main/java/com/timoxino/interview/web/repo/QuestionCategoryNodeRepository.java
@@ -1,10 +1,17 @@
 package com.timoxino.interview.web.repo;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.timoxino.interview.web.model.QuestionCategoryNode;
 
 public interface QuestionCategoryNodeRepository extends Neo4jRepository<QuestionCategoryNode, UUID> {
+    @Query("MATCH(category:QUESTION_CATEGORY_NODE) <-[:CLASSIFIED_AS]-(question:DATA_NODE)\n" + //
+    "WHERE question.uuid = $questionUuid\n" + //
+    "RETURN category")
+    List<QuestionCategoryNode> findCategoryByQuestion(@Param("questionUuid") String questionUuid);
 }
--- src/main/java/com/timoxino/interview/web/repo/QuestionComplexityNodeRepository.java
+++ src/main/java/com/timoxino/interview/web/repo/QuestionComplexityNodeRepository.java
@@ -1,10 +1,17 @@
 package com.timoxino.interview.web.repo;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.timoxino.interview.web.model.QuestionComplexityNode;
 
 public interface QuestionComplexityNodeRepository extends Neo4jRepository<QuestionComplexityNode, UUID> {
+    @Query("MATCH(complexity:QUESTION_COMPLEXITY_NODE) <-[:GRADED_AS]-(question:DATA_NODE)\n" + //
+    "WHERE question.uuid = $questionUuid\n" + //
+    "RETURN complexity")
+    List<QuestionComplexityNode> findComplexitiesByQuestion(@Param("questionUuid") String questionUuid);
 }

510c897c9dd405eaf7c17d2ceb78ac518b0ac6c6
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-08-08 17:32:12 -0400

    fix cypher to fetch all relations

--- src/main/java/com/timoxino/interview/web/repo/QuestionCategoryNodeRepository.java
+++ src/main/java/com/timoxino/interview/web/repo/QuestionCategoryNodeRepository.java
@@ -8,8 +8,9 @@ import org.springframework.data.repository.query.Param;
 import com.timoxino.interview.web.model.QuestionCategoryNode;
 
 public interface QuestionCategoryNodeRepository extends QuestionAwareRepository<QuestionCategoryNode> {
-    @Query("MATCH(category:QUESTION_CATEGORY_NODE) <-[:CLASSIFIED_AS]-(question:DATA_NODE)\n" + //
-            "WHERE question.uuid = $questionUuid\n" + //
-            "RETURN category")
+    @Query("MATCH(category:QUESTION_CATEGORY_NODE)<-[:CLASSIFIED_AS]-(:DATA_NODE {uuid: $questionUuid})\n" + //
+            "MATCH(category)-[graded:CLASSIFIED_AS]-(question:DATA_NODE)-[belongs:BELONGS_TO*]->(topic:DATA_NODE)\n" + //
+            "OPTIONAL MATCH (topic)-[held:HELD_BY]->(role:ROLE_NODE)\n" + //
+            "RETURN category, collect(graded), collect(question), collect(belongs), collect(topic), collect(held), collect(role)")
     List<QuestionCategoryNode> findByQuestion(@Param("questionUuid") String questionUuid);
 }
--- src/main/java/com/timoxino/interview/web/repo/QuestionComplexityNodeRepository.java
+++ src/main/java/com/timoxino/interview/web/repo/QuestionComplexityNodeRepository.java
@@ -8,8 +8,9 @@ import org.springframework.data.repository.query.Param;
 import com.timoxino.interview.web.model.QuestionComplexityNode;
 
 public interface QuestionComplexityNodeRepository extends QuestionAwareRepository<QuestionComplexityNode> {
-    @Query("MATCH(complexity:QUESTION_COMPLEXITY_NODE) <-[:GRADED_AS]-(question:DATA_NODE)\n" + //
-            "WHERE question.uuid = $questionUuid\n" + //
-            "RETURN complexity")
+    @Query("MATCH(complexity:QUESTION_COMPLEXITY_NODE)<-[:GRADED_AS]-(:DATA_NODE {uuid: $questionUuid})\n" + //
+            "MATCH(complexity)-[graded:GRADED_AS]-(question:DATA_NODE)-[belongs:BELONGS_TO*]->(topic:DATA_NODE)\n" + //
+            "OPTIONAL MATCH (topic)-[held:HELD_BY]->(role:ROLE_NODE)\n" + //
+            "RETURN complexity, collect(graded), collect(question), collect(belongs), collect(topic), collect(held), collect(role)")
     List<QuestionComplexityNode> findByQuestion(@Param("questionUuid") String questionUuid);
 }

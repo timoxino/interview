c5bc3379bd9d28e0c2a66482df055bff72509d15
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-08-15 10:03:48 -0400

    add unit tests for create question endpoint

--- src/test/java/com/timoxino/interview/web/controller/QuestionControllerTest.java
+++ src/test/java/com/timoxino/interview/web/controller/QuestionControllerTest.java
@@ -16,13 +16,18 @@ import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.timoxino.interview.web.dto.QuestionCreateRequest;
 import com.timoxino.interview.web.dto.QuestionUpdateRequest;
+import com.timoxino.interview.web.exception.DuplicateNodeNameException;
 import com.timoxino.interview.web.exception.MissingIdException;
 import com.timoxino.interview.web.exception.ObjectNotFoundException;
+import com.timoxino.interview.web.exception.ParentDetailsMissingException;
 import com.timoxino.interview.web.model.DataNode;
 import com.timoxino.interview.web.model.QuestionCategoryNode;
 import com.timoxino.interview.web.model.QuestionComplexityNode;
@@ -43,6 +48,68 @@ public class QuestionControllerTest {
 
     @InjectMocks
     QuestionController controller;
+
+    @Captor
+    ArgumentCaptor<QuestionCategoryNode> categoryCaptor;
+
+    @Captor
+    ArgumentCaptor<QuestionComplexityNode> complexityCaptor;
+
+    @Test
+    void createQuestionMisssingUuid() {
+        QuestionCreateRequest request = new QuestionCreateRequest();
+        assertThrows(MissingIdException.class, () -> {
+            controller.createQuestion(request);
+        }, "Method must throw MissingIdException when no 'uuid' passed");
+
+        request.setCategoryUuid("d0714a68-8755-476e-bdb6-6ff8138cd22c");
+        assertThrows(MissingIdException.class, () -> {
+            controller.createQuestion(request);
+        }, "Method must throw MissingIdException when no 'uuid' passed");
+    }
+
+    @Test
+    void createQuestionMissingObject() throws ObjectNotFoundException, MissingIdException {
+        QuestionCreateRequest request = new QuestionCreateRequest();
+        request.setComplexityUuid("d0714a68-8755-476e-bdb6-6ff8138cd22c");
+        request.setCategoryUuid("d0714a68-8755-476e-bdb6-6ff8138cd22c");
+
+        when(questionCategoryNodeRepository.existsById(any(UUID.class))).thenReturn(false);
+
+        assertThrows(ObjectNotFoundException.class, () -> {
+            controller.createQuestion(request);
+        }, "Method must return ObjectNotFoundException when no object found by 'id'");
+    }
+
+    @Test
+    void createQuestion() throws MissingIdException, ParentDetailsMissingException, DuplicateNodeNameException {
+        QuestionCreateRequest request = new QuestionCreateRequest();
+        request.setComplexityUuid("d0714a68-8755-476e-bdb6-6ff8138cd22c");
+        request.setCategoryUuid("fc6d6e00-a10f-4809-b8d6-0ef3ee344498");
+        DataNode question = DataNode.builder().uuid(UUID.fromString("3c934ee3-641b-43c3-ae82-a90e070d9561")).build();
+        request.setDataNode(question);
+        QuestionCategoryNode questionCategoryNode = QuestionCategoryNode.builder().questions(new ArrayList<>()).build();
+        QuestionComplexityNode questionComplexityNode = QuestionComplexityNode.builder().questions(new ArrayList<>())
+                .build();
+
+        when(questionCategoryNodeRepository.existsById(any(UUID.class))).thenReturn(true);
+        when(questionComplexityNodeRepository.existsById(any(UUID.class))).thenReturn(true);
+        when(dataController.create(question)).thenReturn(question);
+        when(questionCategoryNodeRepository.findById(UUID.fromString("fc6d6e00-a10f-4809-b8d6-0ef3ee344498")))
+                .thenReturn(Optional.of(questionCategoryNode));
+        when(questionComplexityNodeRepository.findById(UUID.fromString("d0714a68-8755-476e-bdb6-6ff8138cd22c")))
+                .thenReturn(Optional.of(questionComplexityNode));
+
+        controller.createQuestion(request);
+
+        verify(questionCategoryNodeRepository).save(categoryCaptor.capture());
+        verify(questionComplexityNodeRepository).save(complexityCaptor.capture());
+
+        assertFalse(categoryCaptor.getValue().getQuestions().isEmpty(),
+                "Question should have been added to the Category");
+        assertFalse(complexityCaptor.getValue().getQuestions().isEmpty(),
+                "Question should have been added to the Complexity");
+    }
 
     @Test
     void updateQuestionMisssingUuid() throws ObjectNotFoundException, MissingIdException {

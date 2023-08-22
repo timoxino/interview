3279edebe5d50f16270f50383aab9b9182ad03f0
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-08-22 12:40:14 +0000

    add configuration for input channel

---
+++ src/main/java/com/timoxino/interview/web/spring/PubSubConfiguration.java
@@ -0,0 +1,53 @@
+package com.timoxino.interview.web.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.PublishSubscribeChannel;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.handler.annotation.Header;
+
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import com.google.cloud.spring.pubsub.integration.AckMode;
+import com.google.cloud.spring.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import com.google.cloud.spring.pubsub.support.BasicAcknowledgeablePubsubMessage;
+import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders;
+
+@Configuration
+public class PubSubConfiguration {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(PubSubConfiguration.class);
+
+    // Create a message channel for messages arriving from the subscription
+    // `sub-one`.
+    @Bean
+    public MessageChannel inputMessageChannel() {
+        return new PublishSubscribeChannel();
+    }
+
+    // Create an inbound channel adapter to listen to the subscription `sub-one` and
+    // send
+    // messages to the input message channel.
+    @Bean
+    public PubSubInboundChannelAdapter inboundChannelAdapter(
+            @Qualifier("inputMessageChannel") MessageChannel messageChannel,
+            PubSubTemplate pubSubTemplate) {
+        PubSubInboundChannelAdapter adapter = new PubSubInboundChannelAdapter(pubSubTemplate, "sub-one");
+        adapter.setOutputChannel(messageChannel);
+        adapter.setAckMode(AckMode.MANUAL);
+        adapter.setPayloadType(String.class);
+        return adapter;
+    }
+
+    // Define what happens to the messages arriving in the message channel.
+    @ServiceActivator(inputChannel = "inputMessageChannel")
+    public void messageReceiver(
+            String payload,
+            @Header(GcpPubSubHeaders.ORIGINAL_MESSAGE) BasicAcknowledgeablePubsubMessage message) {
+        LOGGER.warn("Message arrived via an inbound channel adapter from sub-one! Payload: " + payload);
+        message.ack();
+    }
+}

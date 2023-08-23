b29ecdb723d8f969777694ecbaf12bf5dcc53d4f
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-08-23 14:11:19 -0400

    call super for toString

--- src/main/java/com/timoxino/interview/web/dto/CandidateEstimatedMessage.java
+++ src/main/java/com/timoxino/interview/web/dto/CandidateEstimatedMessage.java
@@ -2,9 +2,11 @@ package com.timoxino.interview.web.dto;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class CandidateEstimatedMessage extends CandidateBaseMessage {
     private Integer lvlEstimated;
 }
--- src/main/java/com/timoxino/interview/web/dto/CandidateQuestionsMessage.java
+++ src/main/java/com/timoxino/interview/web/dto/CandidateQuestionsMessage.java
@@ -5,9 +5,11 @@ import com.timoxino.interview.web.model.DataNode;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class CandidateQuestionsMessage extends CandidateEstimatedMessage {
     private Multimap<String, DataNode> questions;
 }

86cdbc5695edf6efac95dc0d82faa7009052dc1e
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-08-23 14:11:19 -0400

    call super for toString

--- src/main/java/com/timoxino/interview/web/dto/CandidateExtractedSkillsMessage.java
+++ src/main/java/com/timoxino/interview/web/dto/CandidateExtractedSkillsMessage.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class CandidateExtractedSkillsMessage extends CandidateEstimatedMessage {
     private List<String> skills;
 }

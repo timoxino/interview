898bf1baf5668420e734ac283768b3a8891e2fed
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-08-24 15:05:30 -0400

    write to topic after message received

--- src/main/java/com/timoxino/interview/web/spring/PubSubReceiverConfiguration.java
+++ src/main/java/com/timoxino/interview/web/spring/PubSubReceiverConfiguration.java
@@ -2,6 +2,7 @@ package com.timoxino.interview.web.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,12 +20,17 @@ import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders;
 import com.google.cloud.spring.pubsub.support.converter.JacksonPubSubMessageConverter;
 import com.timoxino.interview.web.annotation.GcpCloudRun;
 import com.timoxino.interview.web.dto.CandidateExtractedSkillsMessage;
+import com.timoxino.interview.web.dto.CandidateQuestionsMessage;
+import com.timoxino.interview.web.spring.PubSubSenderConfiguration.PubSubQuestionsGateway;
 
 @Configuration
 @GcpCloudRun
 public class PubSubReceiverConfiguration {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(PubSubReceiverConfiguration.class);
+
+    @Autowired
+    PubSubQuestionsGateway pubSubGateway;
 
     @Bean
     public JacksonPubSubMessageConverter jacksonPubSubMessageConverter(ObjectMapper objectMapper) {
@@ -63,7 +69,10 @@ public class PubSubReceiverConfiguration {
     public void messageReceiver(
             CandidateExtractedSkillsMessage payload,
             @Header(GcpPubSubHeaders.ORIGINAL_MESSAGE) BasicAcknowledgeablePubsubMessage message) {
-        LOGGER.warn("Message arrived via an inbound channel adapter from sub-one! Payload: " + payload.toString());
+        LOGGER.warn("Message arrived from 'extracted_skills_topic'. Payload: " + payload.toString());
         message.ack();
+        CandidateQuestionsMessage messageToSend = new CandidateQuestionsMessage();
+        messageToSend.setRole("New Role");
+        pubSubGateway.sendQuestionsToPubSub(messageToSend);
     }
 }

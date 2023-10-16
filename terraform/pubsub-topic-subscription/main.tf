8d13182a1266e0f88f03b224a6b891ef140a08c0
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-10-16 16:55:18 -0400

    set ack_deadline_seconds to 600

--- terraform/pubsub-topic-subscription/main.tf
+++ terraform/pubsub-topic-subscription/main.tf
@@ -6,4 +6,5 @@ resource "google_pubsub_subscription" "subscription_sage" {
   name  = "sage_subscription"
   topic = google_pubsub_topic.topic_extracted_skills.name
   enable_exactly_once_delivery = true
+  ack_deadline_seconds = 600
 }

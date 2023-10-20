503c635eb697eb7aff0c57467bfbd6d3e38d1ab7
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-10-20 17:05:18 -0400

    deliver once

--- terraform/pubsub-topic-subscription/main.tf
+++ terraform/pubsub-topic-subscription/main.tf
@@ -6,4 +6,5 @@ resource "google_pubsub_subscription" "subscription_sage" {
   name  = "sage_subscription"
   topic = google_pubsub_topic.topic_extracted_skills.name
   ack_deadline_seconds = 600
+  enable_exactly_once_delivery = true
 }
